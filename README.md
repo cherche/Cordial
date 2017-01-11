28646e2754a19696d28b5760fc8cb1bb7173df76
Loquacious <Loquacious@users.noreply.github.com>
2017-01-11 18:04:49 -0500

    Complete rewrite in ES6 and changes in usage of script

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Now just link to it in your HTML file like so:
 <script src="cordial.js"></script>
 ```
 
-## Usage
+## Usage (*Outdated!*)
 Throughout this documentation, `window.Cordial` will refer to the function used to create a version of `Cordial`. The two should not be .
 
 ### `window.Cordial()`
@@ -63,6 +63,36 @@ A constructor hidden from the global scope. Is used to create a new module.
 A method in `Module`'s prototype. Is used to install triggers.
 
 *triggers*: Array, should contain triggers
+
+### What is a "trigger"?
+A trigger consists of two mandatory parts and two optional. These are: `text`, `response`, `post`, and `type`.
+
+`text` can be a string, a regular expression, or an array that may contain any combination of them.
+`response` can be a string, a function, or an array.
+	- If a string or an HTMLElement, it will return that*
+	- If a function, it evaluations the function with no arguments and returns that*
+	- If an array, it picks a random element from it and returns that*
+`post` must be an array or not defined at all.
+`type` must be a string. If it's neither `'startsWith'` nor `'endsWith'`, it will default to checking the user input *exactly*. `type` won't affect anything if `text` is a regular expression.
+
+\* The process will repeat until a string or HTMLElement is returned. This is to allow nesting and to ensure that a string or an HTMLElement is returned.
+The following example will return a random element from the jagged array that's returned from a function.
+```javascript
+[{
+	text: 'something',
+	response: function () {
+		return [
+			[document.createElement('a')],
+			['test', 'wow'],
+			['hello', function () { return 'dog' }, 'world']
+		]
+	}
+}, {
+	// other trigger here
+} /*
+	maybe more triggers here
+*/]
+```
 
 ## Contributing
 Since I lack time for testing, if you come across anything you would like added into Cordial, just [open up and issue](https://github.com/Loquacious/Cordial/issues/new) or add it yourself and [submit a pull request](https://github.com/Loquacious/Cordial/compare) if you really feel like it!
--- cordial.js
+++ cordial.js
@@ -1,154 +1,66 @@
-;(function (w) {
-	'use strict';
+function Cordial() {
+	function c(input) {
+		const parsed = c.parse(input);
 
-	function Module() {
-		this.triggers = [];
-		this.enabled = true;
-	}
+		let category,
+			match = false,
+			finalTemplate;
 
-	Module.prototype.install = function (triggers) {
-		var i;
+		for (const key of Object.keys(c.categories)) {
+			category = c.categories[key];
 
-		if (!Array.isArray(triggers)) {
-			throw new TypeError('(Cordial) The triggers variable must be an array.');
-		}
+			for (let {pattern, template, post, type} of category) {
+				for (const value of pattern) {
+					switch (type) {
+					case 'startsWith':
+					case 'endsWith':
+						match = parsed[type](value);
+						break;
+					case 'isEqualTo':
+						match = parsed === value;
+					}
 
-		if (this.triggers === []) {
-			this.triggers = triggers;
-		} else {
-			for (i = 0; i < triggers.length; i++) {
-				this.triggers.push(triggers[i]);
-			}
-		}
+					if (match) {
+						finalTemplate = template;
 
-		// Run through each trigger within the module
-		for (i = 0; i < this.triggers.length; i++) {
-			this.triggers[i].text =
-				(Array.isArray(this.triggers[i].text)) ?
-				this.triggers[i].text :
-				[this.triggers[i].text];
-
-			this.triggers[i].type =
-				this.triggers[i].type || 'startsWith';
-
-			this.triggers[i].post =
-				(typeof this.triggers[i].post !== 'string') ?
-				'' :
-				this.triggers[i].post;
-		}
-
-		return this;
-	};
-
-	Module.prototype.toggle = function () {
-		this.enabled = !this.enabled;
-
-		return this;
-	};
-
-	w.Cordial = function () {
-		function Cordial(raw) {
-			if (!raw) {
-				return null;
-			}
-
-			var parsed = Cordial.utilities.parse(raw),
-				key,
-				mod,
-				i,
-				j,
-				match,
-				response;
-
-			// Run through each module
-			for (key in Cordial.modules) {
-				if (Cordial.modules.hasOwnProperty(key)) {
-					mod = Cordial.modules[key];
-					// Test if module is enabled before going through triggers
-					if (mod.enabled) {
-						// Run through each trigger within the module
-						for (i = 0; i < mod.triggers.length; i++) {
-							// Run through all values in the 'text' array
-							for (j = 0; j < mod.triggers[i].text.length; j++) {
-								if (typeof mod.triggers[i].text[j] === 'string') {
-									if (mod.triggers[i].type === 'startsWith') {
-										match = (parsed).startsWith(mod.triggers[i].text[j]);
-									} else {
-										match = parsed === mod.triggers[i].text[j];
-									}
-								} else {
-									// In the case that the value is a regular expression
-									match = !!parsed.match(mod.triggers[i].text[j]);
-								}
-
-								if (match) { break; }
+						while (!(
+							typeof finalTemplate === 'string' ||
+							Cordial.isElement(finalTemplate)
+						)) {
+							if (Array.isArray(finalTemplate)) {
+								finalTemplate = Cordial.getRandomValue(finalTemplate);
+							} else if (typeof finalTemplate === 'function') {
+								finalTemplate = finalTemplate(parsed);
 							}
-
-							if (match) { break; }
 						}
 
-						if (match) { break; }
+						if (typeof finalTemplate === 'string') {
+							finalTemplate += Cordial.getRandomValue(post);
+						}
+
+						return finalTemplate;
 					}
 				}
-			}
-
-			if (match) {
-				response = mod.triggers[i].response;
-
-				while (Array.isArray(response) || typeof response === 'function') {
-					if (Array.isArray(response)) {
-						response = response[Math.floor(Math.random() * response.length)];
-					} else if (typeof response === 'function') {
-						response = response(parsed);
-					}
-				}
-
-				if (typeof response === 'string') {
-					response += mod.triggers[i].post.charAt(Math.floor(Math.random() * mod.triggers[i].post.length));
-				} else if (!Cordial.utilities.isElement(response)) {
-					throw new TypeError('(Cordial) Responses must always return a string or HTMLElement.');
-				}
-
-				return response;
-			} else {
-				return Cordial.fallback(parsed);
 			}
 		}
+	}
 
-		Cordial.utilities = {
-			parse: function (raw) {
-				return raw
-					.toLowerCase()
-					.replace(/(\?|!|,|"|')+/g, '')
-					.replace(/^\s+|(\.|\s)+$/g, '')
-					.replace(/\s+/g, ' ');
-			},
-			// http://stackoverflow.com/a/384380
-			isElement: function (obj) {
-				return (
-					typeof HTMLElement === 'object' ? obj instanceof HTMLElement :
-					obj && typeof obj === 'object' && obj !== null && obj.nodeType === 1 && typeof obj.nodeName === 'string'
-				);
-			}
-		};
-
-		Cordial.modules = {
-			core: new Module()
-		};
-
-		Cordial.createModule = function (name) {
-			this.modules[name] = new Module();
-			return this.modules[name];
-		};
-
-		Cordial.install = function (triggers) {
-			this.modules.core.install(triggers);
-		};
-
-		Cordial.fallback = function () {
-			return null;
-		};
-
-		return Cordial;
+	c.categories = {
+		core: []
 	};
-})(window);
+
+	c.parse = x => x
+		.toLowerCase()
+		.replace(/(\?|!|,|"|')+|^(\.|\s)+|(\.|\s)+$/g, '')
+		.replace(/\s+/g, ' ');
+
+	c.fallback = () => null;
+
+	return c;
+}
+
+Cordial.isElement = obj =>
+		typeof HTMLElement === 'object' ? obj instanceof HTMLElement :
+		obj && typeof obj === 'object' && obj !== null && obj.nodeType === 1 && typeof obj.nodeName === 'string';
+
+Cordial.getRandomValue = arr => arr[Math.floor(Math.random() * arr.length)];

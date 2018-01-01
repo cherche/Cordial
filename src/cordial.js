94a3ae47f747a28e92ad1ca3b19493158cf5a90b
Ryan Nguyen <contactryannguyen@gmail.com>
2018-01-01 12:10:54 -0500

    Refactor to make purpose of each method clearer

--- src/cordial.js
+++ src/cordial.js
@@ -1,45 +1,40 @@
 import { getRandomVal } from './probability.js'
 
-function Cordial (categories = []) {
+export default function Cordial (categories = []) {
+  // The CordialInstance can also be instantiated with an array of
+  // categories passed in as the first argument
   const CordialInstance = { categories }
 
-  CordialInstance.getTemplates = function getTemplates (input) {
-    for (const category of CordialInstance.categories) {
-      const { patterns, templates } = category
+  CordialInstance.checkPatterns = function checkPatterns (patterns, input) {
+    let checker
 
-      let checker
-
-      // Check `matches` differently depending on what the `type` is
-      switch (patterns.type) {
-        case 'equals':
-          checker = match => input === match
-          break
-        case 'includes':
-          checker = match => input.includes(match)
-          break
-        case 'startsWith':
-          checker = match => input.startsWith(match)
-          break
-        case 'endsWith':
-          checker = match => input.endsWith(match)
-          break
-        case 'regex':
-          checker = match => input.match(match).length > 0
-      }
-
-      // Iterate over the matches, and return as soon as a valid match is found
-      for (const match of patterns.matches) {
-        const isMatch = checker(match)
-
-        if (isMatch) return templates
-      }
+    // Check `matches` differently depending on what the `type` is
+    switch (patterns.type) {
+      case 'equals':
+        checker = match => input === match
+        break
+      case 'includes':
+        checker = match => input.includes(match)
+        break
+      case 'startsWith':
+        checker = match => input.startsWith(match)
+        break
+      case 'endsWith':
+        checker = match => input.endsWith(match)
+        break
+      case 'regex':
+        checker = match => input.match(match).length > 0
     }
 
-    // Default value
-    return null
+    // Iterate over the matches, and return as soon as a valid match is found
+    for (const match of patterns.matches) {
+      const isMatch = checker(match)
+
+      if (isMatch) return true
+    }
   }
 
-  CordialInstance.reduceTemplates = function reduceTemplates (templates, input) {
+  CordialInstance.processTemplates = function processTemplates (templates, input) {
     const { responses, trails, type } = templates
 
     let response
@@ -56,12 +51,19 @@ function Cordial (categories = []) {
   }
 
   CordialInstance.tell = function tell (input) {
-    const templates = CordialInstance.getTemplates(input)
+    for (const category of CordialInstance.categories) {
+      const { patterns, templates } = category
 
-    return CordialInstance.reduceTemplates(templates, input)
+      if (CordialInstance.checkPatterns(patterns, input)) {
+        const template = CordialInstance.processTemplates(templates, input)
+
+        return template
+      }
+    }
+
+    // Default value
+    return null
   }
 
   return CordialInstance
 }
-
-export default Cordial
